1e45c2100362bc193a2fc811bf7fd35ffe8e096d
Chris Phillips <christopher.trevor.phillips@gmail.com>
2024-10-13 15:07:49 -0700

    Fill out Game::Initialize

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include <SDL2/SDL.h>
 #include <iostream>
 
 Game::Game(){
@@ -10,7 +11,35 @@ Game::~Game(){
 }
 
 void Game::Initialize(){
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0){                                             // initialize SDL and catch errors
+        std::cerr << "Error installing SDL." << std::endl;
+        return;
+    }
 
+    SDL_Window* window = SDL_CreateWindow(                                              // create SDL window instance
+        NULL,                                                                           // window title
+        SDL_WINDOWPOS_CENTERED,                                                         // x position
+        SDL_WINDOWPOS_CENTERED,                                                         // y position
+        800,                                                                            // width
+        600,                                                                            // height
+        SDL_WINDOW_BORDERLESS                                                           // flags
+    );
+
+    if(!window){                                                                        // catch window errors
+        std::cerr << "Error creating SDL window" << std::endl;
+        return;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(                                        // create SDL renderer to go inside SDL window
+        window,                                                                         // associated window
+        -1,                                                                             // associated display (-1 = default)
+        0                                                                               // flags
+    );
+
+    if(!renderer){                                                                      // catch renderer errors
+        std::cerr << "Error creating SDL renderer." << std::endl;
+        return;
+    }
 }
 
 void Game::Run(){
