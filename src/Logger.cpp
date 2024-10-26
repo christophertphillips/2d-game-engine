e26291223ec1a44ebfad0a32a07e74ed233422b9
Chris Phillips <christopher.trevor.phillips@gmail.com>
2024-10-26 11:40:30 -0700

    Add curDateTimeStr() in Logger.cpp

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,4 +1,19 @@
 #include "Logger.h"
+#include <ctime>
+
+std::string curDateTimeStr(){
+    // alternate method using chrono
+    // std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    // std::string output(30, '\0');
+    // std::strftime(&output[0], output.size(), "%d-%b-%Y %H:%M:%S", std::localtime(&now));
+
+    std::time_t timestamp = std::time(NULL);                                            // create timestamp for current day/time
+    struct std::tm datetime = *std::localtime(&timestamp);                              // convert timestamp to tm struct in local time zone
+    char output[30];                                                                    // create string to hold formatted datetime string
+    std::strftime(output, 30, "%d-%b-%Y %H:%M:%S", &datetime);                          // format datetime string
+
+    return output;                                                                      // return formatted datetime string
+}
 
 void Logger::Log(const std::string& message){
 
