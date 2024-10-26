beb18d13adeb865c65ea696984d0c300041b7fe4
Chris Phillips <christopher.trevor.phillips@gmail.com>
2024-10-26 12:49:58 -0700

    Add enum LogType, struct LogEntry to Logger.h

--- src/Logger.h
+++ src/Logger.h
@@ -3,6 +3,17 @@
 
 #include <string>
 
+enum LogType{
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR
+};
+
+struct LogEntry{
+    LogType type;
+    std::string message;
+};
+
 class Logger{
     public:
         static void Log(const std::string& message);
