3dafc23aa8dfd5cc5b1308d5ec570f686b3981d8
Chris Phillips <christopher.trevor.phillips@gmail.com>
2024-11-03 14:36:43 -0800

    Add Signature bitset type to ECS.h

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -1,6 +1,12 @@
 #ifndef ECS_H
 #define ECS_H
 
+#include <bitset>
+
+// (the bitset defines (1) which components an entity has, and (2) which components a system is interested in)
+const unsigned int MAX_COMPONENTS = 32;                                                 // define bitset length = 32 bits
+typedef std::bitset<MAX_COMPONENTS> Signature;                                          // define 32-bit bitset type
+
 class Component{
 
 };
