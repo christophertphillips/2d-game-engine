49d49a3f5fcc978606f6e059efb18047bdec0af3
Chris Phillips <christopher.trevor.phillips@gmail.com>
2024-11-03 14:23:49 -0800

    Add GetID() to ECS.cpp

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,1 +1,5 @@
 #include "ECS.h"
+
+int Entity::GetId() const {                                                             // (const indicates method doesn't change members)
+    return id;                                                                          // return entity's id
+}
