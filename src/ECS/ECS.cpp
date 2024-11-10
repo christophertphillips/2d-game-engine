33e5ac24576b04b7493655ab9f95354c5704bc9b
Chris Phillips <christopher.trevor.phillips@gmail.com>
2024-11-10 09:31:28 -0800

    Implement System::GetSystemEntities()

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -3,3 +3,7 @@
 int Entity::GetId() const {                                                             // (const indicates method doesn't change members)
     return id;                                                                          // return entity's id
 }
+
+std::vector<Entity> System::GetSystemEntities() const{
+    return entities;                                                                    // return vector of entities associated with system
+};
