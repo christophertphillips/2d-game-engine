0932762cf8bd9024e9a959877aa1568b2e55722e
Chris Phillips <christopher.trevor.phillips@gmail.com>
2024-11-10 09:50:47 -0800

    Implement System::RemoveEntityFromSystem()

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -8,6 +8,24 @@ void System::AddEntityToSystem(Entity entity){
     entities.push_back(entity);                                                         // add entity to system's entities vector
 };
 
+void System::RemoveEntityFromSystem(Entity entity){
+    // my original implementation
+    // for(int index = 0; index < entities.size(); index++){
+    //     if(entities.at(index).GetId() == entity.GetId()){
+    //         entities.erase(entities.begin()+index);
+    //         return;
+    //     }
+    // }
+    entities.erase(                                                                     // removes entity using returned iterator and iterator of last element
+        std::remove_if(                                                                 // returns iterator to first matching elmenet (in this case, only one is possible)
+            entities.begin(),
+            entities.end(),
+            [&entity](Entity other) { return entity.GetId() == other.GetId(); }         // lambda function that matches 'other' entity to 'entity' entity
+        ),
+        entities.end()                                                                  // iterator of last element
+    );
+};
+
 std::vector<Entity> System::GetSystemEntities() const{
     return entities;                                                                    // return vector of entities associated with system
 };
