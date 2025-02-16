c9a9dbd424a1bd7141357a85b2f6de3c03790b7e
Chris Phillips <christopher.trevor.phillips@gmail.com>
2025-02-16 11:25:02 -0800

    Use scale factor in BoxColliderComponent

--- src/Components/BoxColliderComponent.h
+++ src/Components/BoxColliderComponent.h
@@ -8,9 +8,9 @@ struct BoxColliderComponent{
     int height;
     glm::vec2 offset;
 
-    BoxColliderComponent(int width = 0, int height = 0, glm::vec2 offset = glm::vec2(0)){
-        this->width = width;
-        this->height = height;
+    BoxColliderComponent(int width = 0, int height = 0, glm::vec2 scale = glm::vec2(1.0, 1.0), glm::vec2 offset = glm::vec2(0)){
+        this->width = width * scale.x;
+        this->height = height * scale.y;
         this->offset = offset;
     }
 };
