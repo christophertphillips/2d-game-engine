f585b144e52a2189af04895b584c60d70ed235fe
Chris Phillips <christopher.trevor.phillips@gmail.com>
2025-04-12 20:33:56 -0700

    Add fonts member to AssetStore

--- src/AssetStore/AssetStore.h
+++ src/AssetStore/AssetStore.h
@@ -4,10 +4,12 @@
 #include <map>
 #include <string>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 class AssetStore{
     private:
         std::map<std::string, SDL_Texture*> textures;                                   // map storing textures in [id, texture pointer] pairs
+        std::map<std::string, TTF_Font*> fonts;                                         // map storing fonts in in [id, font pointer] pairs
 
     public:
         AssetStore();                                                                   // AssetStore constructor
