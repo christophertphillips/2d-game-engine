bf93123a16665d6f27db09c99e4ba6f2765b33c7
Chris Phillips <christopher.trevor.phillips@gmail.com>
2025-06-22 08:34:16 -0700

    Run Lua script in LevelLoader::LoadLevel()

--- src/Game/LevelLoader.cpp
+++ src/Game/LevelLoader.cpp
@@ -15,4 +15,19 @@ LevelLoader::~LevelLoader(){
 
 void LevelLoader::LoadLevel(sol::state& lua, SDL_Renderer* renderer, const std::unique_ptr<Registry>& registry, const std::unique_ptr<AssetStore>& assetStore, int levelNumber){
 
+    // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // Load + execute Lua script
+
+    std::string luaScriptPath = "./assets/scripts/Level" + std::to_string(levelNumber) + ".lua";                            // create path for Lua script containing level data
+    sol::load_result luaScriptResult = lua.load_file(luaScriptPath);                                                        // load (but don't execute) Lua script, to check for errors
+
+    if(!luaScriptResult.valid()){                                                                                           // if Lua script has an error...
+        sol::error luaScriptError = luaScriptResult;                                                                        // get the error
+        std::string luaScriptErrorMessage = luaScriptError.what();                                                          // get the message associated with the error
+        Logger::Err("Error loading the Lua script: " + luaScriptErrorMessage);                                              // log the error message
+        return;                                                                                                             // return from LoadLevel (thus stopping further execution)
+    }
+
+    lua.script_file(luaScriptPath);                                                                                         // load (and execute) the Lua script
+
 }
