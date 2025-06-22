47220ebc06a9c1fd9cc4567af380750a73570ed9
Chris Phillips <christopher.trevor.phillips@gmail.com>
2025-06-22 11:01:05 -0700

    Load tilemap via Lua

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -143,55 +143,6 @@ void Game::Setup(){
     lua.open_libraries(sol::lib::base, sol::lib::math);                                 // load libraries into Lua virtual machine
     levelLoader.LoadLevel(lua, renderer, registry, assetStore, 1);                      // load first level
 
-    int tileSize = 32;                                                                  // initialize tile size (32 x 32)
-    double tileScale = 2.0;                                                             // initialize tile scale
-    int tileIndexX = 0;                                                                 // initialize tile x index
-    int tileIndexY = 0;                                                                 // initialize tile y index
-    int tilesAdded = 0;                                                                 // initialize tiles added
-    std::string fileLine;                                                               // delare string to hold current line
-    std::ifstream fileStream("./assets/tilemaps/jungle.map");                           // create input stream from file
-
-    while(std::getline(fileStream, fileLine)){                                          // iterate through lines of file
-
-        std::string fileToken;                                                          // declare string to hold current token from current line
-        std::istringstream fileLineStream(fileLine);                                    // create input stream from current line
-        while(std::getline(fileLineStream, fileToken, ',')){                            // iterate through current line
-
-            int srcRectX = tileSize * std::stoi(fileToken.substr(1,1));                 // calculate source rect x position
-            int srcRectY = tileSize * std::stoi(fileToken.substr(0,1));                 // calculate source rect y position
-            double backgroundTileXPos = tileIndexX * tileSize * tileScale;              // calculate background tile x position
-            double backgroundTileYPos = tileIndexY * tileSize * tileScale;              // calculate background tile y position
-
-            Entity backgroundTile = registry->CreateEntity();                           // create background tile entity
-            backgroundTile.Group("tiles");
-            tilesAdded++;                                                               // increment tiles added
-
-            backgroundTile.AddComponent<TransformComponent>(                            // add transform component to background tile entity
-                glm::vec2(backgroundTileXPos, backgroundTileYPos),                      // position
-                glm::vec2(tileScale, tileScale),                                        // scale
-                0.0                                                                     // rotation
-            );
-            backgroundTile.AddComponent<SpriteComponent>(                               // add sprite component to background tile entity
-                "jungle-map",                                                           // asset id
-                tileSize,                                                               // width
-                tileSize,                                                               // height
-                0,                                                                      // z-index
-                false,                                                                  // isFixed
-                srcRectX,                                                               // source rect x position
-                srcRectY                                                                // source rect y position
-            );
-
-            tileIndexX++;                                                               // increase tile x index
-        }
-
-        tileIndexX = 0;                                                                 // reset tile x index
-        tileIndexY++;                                                                   // increase tile y index
-    }
-    fileStream.close();                                                                 // close file stream
-
-    fieldWidth = (tilesAdded / tileIndexY) * tileSize * tileScale;                      // calculate width of field = (num of tiles wide) * (tile size in pixels) * (tile scale)
-    fieldHeight = tileIndexY * tileSize * tileScale;                                    // calculate height of field = (num of tiles tall) * (tile size in pixels) * (tile scale)
-
     Entity chopper = registry->CreateEntity();                                          // create chopper entity
     chopper.Tag("player");                                                              // add 'player' tag to chopper
     chopper.AddComponent<TransformComponent>(glm::vec2(100.0, 100.0), glm::vec2(1.0, 1.0), 0.0);    // add transform component to chopper
--- src/Game/LevelLoader.cpp
+++ src/Game/LevelLoader.cpp
@@ -4,6 +4,11 @@
 #include "../AssetStore/AssetStore.h"
 #include "../Logger/Logger.h"
 #include <sol/sol.hpp>
+#include "./Game.h"
+#include <fstream>
+#include <sstream>
+#include "../Components/TransformComponent.h"
+#include "../Components/SpriteComponent.h"
 
 LevelLoader::LevelLoader(){
     Logger::Log("LevelLoader constructor called!");
@@ -45,5 +50,60 @@ void LevelLoader::LoadLevel(sol::state& lua, SDL_Renderer* renderer, const std::
             assetStore->AddFont(asset["id"], asset["file_path"], asset["font_size"]);                                       // ... load font into the asset store
         }
     }
+
+    // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // Load tilemap
+
+    sol::table tilemap = lua["level"]["tilemap"];                                                                           // load Lua table "level/tilemap"
+    std::string mapFilePath = tilemap["map_file_path"];                                                                     // load map file path
+    std::string textureId = tilemap["texture_id"];                                                                          // load map texture id
+    int tileSize = tilemap["tile_size"];                                                                                    // load tile size
+    double tileScale = tilemap["tile_scale"];                                                                               // load tile scale
+    int tileIndexX = 0;                                                                                                     // initialize tile x index
+    int tileIndexY = 0;                                                                                                     // initialize tile y index
+    int tilesAdded = 0;                                                                                                     // initialize tiles added
+    std::string fileLine;                                                                                                   // delare string to hold current line
+    std::ifstream fileStream("./assets/tilemaps/jungle.map");                                                               // create input stream from file
+
+    while(std::getline(fileStream, fileLine)){                                                                              // iterate through lines of file
+
+        std::string fileToken;                                                                                              // declare string to hold current token from current line
+        std::istringstream fileLineStream(fileLine);                                                                        // create input stream from current line
+        while(std::getline(fileLineStream, fileToken, ',')){                                                                // iterate through current line
+
+            int srcRectX = tileSize * std::stoi(fileToken.substr(1,1));                                                     // calculate source rect x position
+            int srcRectY = tileSize * std::stoi(fileToken.substr(0,1));                                                     // calculate source rect y position
+            double backgroundTileXPos = tileIndexX * tileSize * tileScale;                                                  // calculate background tile x position
+            double backgroundTileYPos = tileIndexY * tileSize * tileScale;                                                  // calculate background tile y position
+
+            Entity backgroundTile = registry->CreateEntity();                                                               // create background tile entity
+            backgroundTile.Group("tiles");
+            tilesAdded++;                                                                                                   // increment tiles added
+
+            backgroundTile.AddComponent<TransformComponent>(                                                                // add transform component to background tile entity
+                glm::vec2(backgroundTileXPos, backgroundTileYPos),                                                          // position
+                glm::vec2(tileScale, tileScale),                                                                            // scale
+                0.0                                                                                                         // rotation
+            );
+            backgroundTile.AddComponent<SpriteComponent>(                                                                   // add sprite component to background tile entity
+                "jungle-map",                                                                                               // asset id
+                tileSize,                                                                                                   // width
+                tileSize,                                                                                                   // height
+                0,                                                                                                          // z-index
+                false,                                                                                                      // isFixed
+                srcRectX,                                                                                                   // source rect x position
+                srcRectY                                                                                                    // source rect y position
+            );
+
+            tileIndexX++;                                                                                                   // increase tile x index
+        }
+
+        tileIndexX = 0;                                                                                                     // reset tile x index
+        tileIndexY++;                                                                                                       // increase tile y index
+    }
+    fileStream.close();                                                                                                     // close file stream
+
+    Game::fieldWidth = (tilesAdded / tileIndexY) * tileSize * tileScale;                                                    // calculate width of field = (num of tiles wide) * (tile size in pixels) * (tile scale)
+    Game::fieldHeight = tileIndexY * tileSize * tileScale;                                                                  // calculate height of field = (num of tiles tall) * (tile size in pixels) * (tile scale)
 
 }
