09904ad2d746122c5582d765c62b89711c6323c7
Chris Phillips <christopher.trevor.phillips@gmail.com>
2025-06-22 08:31:40 -0700

    Add Level1.lua file

---
+++ assets/scripts/Level1.lua
@@ -0,0 +1,14 @@
+level = {
+    assets = {
+        [0] =
+        { type = "texture", id = "jungle-map",    file_path = "./assets/tilemaps/jungle.png"                               },
+        { type = "texture", id = "chopper-image",    file_path = "./assets/images/chopper-spritesheet.png"                 },
+        { type = "texture", id = "tank-image",       file_path = "./assets/images/tank-panther-right.png"                  },
+        { type = "texture", id = "bullet-image",     file_path = "./assets/images/bullet.png"                              },
+        { type = "texture", id = "truck-image",      file_path = "./assets/images/truck-ford-right.png"                    },
+        { type = "texture", id = "radar-image",      file_path = "./assets/images/radar.png"                               },
+        { type = "texture", id = "tree-image",       file_path = "./assets/images/tree.png"                                },
+        { type = "font",    id = "charriot-font",    file_path = "./assets/fonts/charriot.ttf",             font_size = 24 },
+        { type = "font",    id = "charriot-font-12", file_path = "./assets/fonts/charriot.ttf",             font_size = 12 },
+    }
+}
