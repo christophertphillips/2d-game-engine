83b9523f09cd8b44119091ef335f6d40cba144d6
Chris Phillips <christopher.trevor.phillips@gmail.com>
2025-07-18 14:47:56 -0700

    Add SetEntityPosition()

--- src/Systems/ScriptSystem.h
+++ src/Systems/ScriptSystem.h
@@ -3,6 +3,18 @@
 
 #include "../ECS/ECS.h"
 #include "../Components/ScriptComponent.h"
+#include "../Logger/Logger.h"
+
+void SetEntityPosition(Entity entity, double x, double y){
+    if(entity.HasComponent<TransformComponent>()){                                                                          // if entity has a transform component....
+        auto& transformComponent = entity.GetComponent<TransformComponent>();                                               // get entity's transform component
+        transformComponent.position.x = x;                                                                                  // set entity's x position
+        transformComponent.position.y = y;                                                                                  // set entity's y position
+    }
+    else{
+        Logger::Err("Trying to set the position of an entity via Lua that has no transform component.");                    // ... else, log errror message
+    }
+}
 
 class ScriptSystem: public System {
     public:
